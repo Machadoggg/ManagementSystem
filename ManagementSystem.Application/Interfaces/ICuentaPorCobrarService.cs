e55bd7caa2249532f6b9544b731efba6437c12e1
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-17 21:48:36 -0500

    Add ICuentaPorCobrarService interface

---
+++ ManagementSystem.Application/Interfaces/ICuentaPorCobrarService.cs
@@ -0,0 +1,15 @@
+﻿using ManagementSystem.Domain.Entities;
+
+namespace ManagementSystem.Application.Interfaces
+{
+    public interface ICuentaPorCobrarService
+    {
+        Task<List<CuentaPorCobrar>> GetAllAsync();
+        Task<CuentaPorCobrar> GetByIdAsync(int id);
+        Task AddAsync(CuentaPorCobrar cuenta);
+        Task UpdateAsync(CuentaPorCobrar cuenta);
+        Task DeleteAsync(int id);
+        Task<List<CuentaPorCobrar>> GetFilteredReportAsync(DateTime? fechaInicio, DateTime? fechaFin, string cliente);
+        Task<byte[]> ExportToExcelAsync(List<CuentaPorCobrar> cuentas);
+    }
+}
