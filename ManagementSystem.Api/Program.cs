f4b9135e00f94973e63fdf45c9789667255a0543
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-17 22:08:25 -0500

    Configuration dependencies

--- ManagementSystem.Api/Program.cs
+++ ManagementSystem.Api/Program.cs
@@ -1,6 +1,18 @@
+using ManagementSystem.Application.Interfaces;
+using ManagementSystem.Application.Services;
+using ManagementSystem.Infrastructure.Data;
+using ManagementSystem.Infrastructure.Interfaces;
+using ManagementSystem.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICuentaPorCobrarRepository, CuentaPorCobrarRepository>();
+builder.Services.AddScoped<ICuentaPorCobrarService, CuentaPorCobrarService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
