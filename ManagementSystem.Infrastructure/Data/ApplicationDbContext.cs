7881cb29b4e09203900addc81fa834afbeb0b12a
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-17 21:57:13 -0500

    Add ApplicationDbContext

---
+++ ManagementSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ManagementSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManagementSystem.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<CuentaPorCobrar> CuentasPorCobrar { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
