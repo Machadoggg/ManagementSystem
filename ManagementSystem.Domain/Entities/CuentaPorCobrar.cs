b796a0a96d741516a4041840e8ddaa1f0576edd9
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-17 21:46:42 -0500

    Add CuentaPorCobrar entity

---
+++ ManagementSystem.Domain/Entities/CuentaPorCobrar.cs
@@ -0,0 +1,25 @@
+﻿namespace ManagementSystem.Domain.Entities
+{
+    public class CuentaPorCobrar
+    {
+        public int Id { get; set; }
+        public DateTime FechaDocumento { get; set; }
+        public string MesAnio { get; set; } = default!;
+        public string Tipo { get; set; } = default!;
+        public string Documento { get; set; } = default!;
+        public string Cliente { get; set; } = default!;
+        public decimal Valor { get; set; }
+        public decimal NetoR { get; set; }
+        public decimal Cartera { get; set; }
+        public decimal NetoF { get; set; }
+        public decimal IVA { get; set; }
+        public decimal Pagado { get; set; }
+        public DateTime FechaPago { get; set; }
+        public int Dias { get; set; }
+        public decimal EfectivoOtroMeses { get; set; }
+        public decimal Colombia { get; set; }
+        public decimal Davivienda { get; set; }
+        public decimal Bancolombia { get; set; }
+        public decimal Efectivo { get; set; }
+    }
+}
